5c4c40fc3bfe30f4af78af40a9d41845d0f54b75
Connor Clark <connordeclark@gmail.com>
2024-10-08 23:13:22 -0500

    Added basic styles

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.itemContainer}
+      >
+        <Text style={styles.itemText}>Coffee</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center",
   },
+  itemContainer: {
+    borderBottomWidth: 1, //not a pixel, but a display point
+    borderBottomColor: "#1a759f", 
+    paddingHorizontal: 8, 
+    paddingVertical: 16
+    },
+  itemText: { 
+    fontSize: 18, 
+    fontWeight: 200
+  }
 });
---
+++ theme.ts
@@ -0,0 +1,4 @@
+export const theme = {
+    colorCerulea: "#1a759f",
+    colorWhite: "#fff"
+};
